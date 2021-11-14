acaef9250542dec3349116ea8c56986906aad207
Damir Makhmutov <damir.makhmutov@cleverlabs.io>
2021-11-14 17:11:10 +0400

    Add schema registry

--- auth/app/controllers/registrations_controller.rb
+++ auth/app/controllers/registrations_controller.rb
@@ -4,10 +4,20 @@ class RegistrationsController < Devise::RegistrationsController
     resource.update!(role: :admin)
 
     event = {
+      event_id: SecureRandom.uuid,
+      event_version: "1",
       event_name: 'AccountCreated',
+      event_time: Time.now.to_s,
+      producer: 'auth_service',
       data: { public_id: resource.public_id, email: resource.email, role: resource.role }
     }
 
-    KafkaProducer.produce_sync(topic: 'accounts-stream', payload: event.to_json)
+    result = SchemaRegistry.validate_event(event, 'accounts.created', version: 1)
+
+    if result.success?
+      KafkaProducer.produce_sync(topic: 'accounts-stream', payload: event.to_json)
+    else
+      raise result.failure
+    end
   end
 end
